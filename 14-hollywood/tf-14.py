f566ed9063eb2431c9e68b407ebaa44e414fb867
rogera <wolfgang.schuessel@gmail.com>
2016-04-28 14:48:20 +0200

    runnable

--- 14-hollywood/tf-14.py
+++ 14-hollywood/tf-14.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys, re, operator, string
 
+
 #
 # The "I'll call you back" Word Frequency Framework
 #
@@ -17,7 +18,7 @@ class WordFrequencyFramework:
 
     def register_for_end_event(self, handler):
         self._end_event_handlers.append(handler)
-    
+
     def run(self, path_to_file):
         for h in self._load_event_handlers:
             h(path_to_file)
@@ -25,6 +26,7 @@ class WordFrequencyFramework:
             h()
         for h in self._end_event_handlers:
             h()
+
 
 #
 # The entities of the application
@@ -58,9 +60,11 @@ class DataStorage:
     def register_for_word_event(self, handler):
         self._word_event_handlers.append(handler)
 
+
 class StopWordFilter:
     """ Models the stop word filter """
     _stop_words = []
+
     def __init__(self, wfapp):
         wfapp.register_for_load_event(self.__load)
 
@@ -73,9 +77,11 @@ class StopWordFilter:
     def is_stop_word(self, word):
         return word in self._stop_words
 
+
 class WordFrequencyCounter:
     """ Keeps the word frequency data """
     _word_freqs = {}
+
     def __init__(self, wfapp, data_storage):
         data_storage.register_for_word_event(self.__increment_count)
         wfapp.register_for_end_event(self.__print_freqs)
@@ -87,9 +93,11 @@ class WordFrequencyCounter:
             self._word_freqs[word] = 1
 
     def __print_freqs(self):
-        word_freqs = sorted(self._word_freqs.iteritems(), key=operator.itemgetter(1), reverse=True)
+        # word_freqs = sorted(self._word_freqs.iteritems(), key=operator.itemgetter(1), reverse=True)
+        word_freqs = sorted(self._word_freqs.items(), key=operator.itemgetter(1), reverse=True)
         for (w, c) in word_freqs[0:25]:
-            print w, ' - ', c
+            print(w, ' - ', c)
+
 
 #
 # The main function
@@ -99,4 +107,3 @@ stop_word_filter = StopWordFilter(wfapp)
 data_storage = DataStorage(wfapp, stop_word_filter)
 word_freq_counter = WordFrequencyCounter(wfapp, data_storage)
 wfapp.run(sys.argv[1])
-
